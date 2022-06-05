8b54870b1e8e09e447c7c3cc81ee26c675c7ae87
Piotr Bęc <becpiotr@gmail.com>
2022-06-05 22:33:31 +0200

    implemented SearchComponent, added DataService, added paths in tsconfig

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DataService } from '@shared/services/data.service';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
-  constructor() {}
+  searchFormControl: FormControl = new FormControl();
+  searchText: string = '';
 
-  ngOnInit(): void {}
+  words: string[] = [];
+  filteredWords?: Observable<string[]>;
+
+  constructor(private dataService: DataService) {
+    this.dataService.getAllData().subscribe((data) => {
+      this.words = data;
+    });
+  }
+
+  ngOnInit(): void {
+    this.filteredWords = this.searchFormControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this.getFilteredWords(value || ''))
+    );
+  }
+
+  getFilteredWords(value: string): string[] {
+    let filterValue = value.toLowerCase();
+    return this.words.filter((word) =>
+      word.toLowerCase().includes(filterValue)
+    );
+  }
+
+  googleSearch() {
+    window.open(
+      `https://google.com/search?q=${encodeURIComponent(this.searchText)}`,
+      '_blank',
+      'noopener'
+    );
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
-const MATERIAL_MODULES = [MatButtonModule];
+const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  FormsModule,
+  MatAutocompleteModule,
+  ReactiveFormsModule,
+];
 
 const SHARED_MODULES = [CommonModule, ...MATERIAL_MODULES];
 const SHARED_COMPONENTS = [];
